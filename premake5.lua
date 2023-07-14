1462635db6d793bf523c3331c31e66a8ddd04b6e
elauganem <edoardo.menaguale@mail.polimi.it>
2023-07-14 15:27:04 +0200

    create project

---
+++ premake5.lua
@@ -0,0 +1,85 @@
+
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "../vendor/GLFW/include"
+IncludeDir["glm"] = "../vendor/glm"
+IncludeDir["other"] = "%{prj.name}/vendor/other"
+IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
+
+LibraryDir = {}
+
+LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+LibraryDir["GLFW"] = "../vendor/GLFW/bin/Debug/GLFW"
+
+
+Library = {}
+
+Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
+
+
+workspace "Project"
+	architecture "x64"
+	
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	startproject "Project"
+
+outputdir = "%{cfg.buildcfg}"
+include "../vendor/GLFW"
+
+project "Project"
+	location "Project"
+	kind "ConsoleApp"	
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines { "_CRT_SECURE_NO_WARNINGS" }
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/other/**.h",
+		"%{prj.name}/vendor/other/**.cpp"
+	}
+
+	includedirs {
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.VulkanSDK}",
+		"%{IncludeDir.other}",
+	}
+
+	libdirs 
+	{ 
+		"%{LibraryDir.VulkanSDK}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"%{Library.Vulkan}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
